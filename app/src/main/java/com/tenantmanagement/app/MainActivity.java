47bf9fa0f00369feca5136663e61826c534611d6
Mahfuz Rahman <mafu@my.yorku.ca>
2022-01-18 21:38:48 -0500

    animation and completeed main activity_main.xml

--- app/src/main/java/com/tenantmanagement/app/MainActivity.java
+++ app/src/main/java/com/tenantmanagement/app/MainActivity.java
@@ -3,14 +3,30 @@ package com.tenantmanagement.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.*;
+import android.widget.Toast;
 import java.util.*;
-
 import model.Property;
 
 public class MainActivity extends AppCompatActivity {
+    /*
+    Floating button animation
+     */
+//    Animation rotateOpen =  AnimationUtils.loadAnimation(this, R.anim.rotate_open_anim);
+//    Animation rotateClose =  AnimationUtils.loadAnimation(this, R.anim.rotate_close_anim);
+//    Animation fromBottom =  AnimationUtils.loadAnimation(this, R.anim.from_bottom_anim);
+//    Animation toBottom =  AnimationUtils.loadAnimation(this, R.anim.to_bottom_anim);
+//    boolean clicked = false;
+//    Button editProperty = findViewById(R.id.floatingActionButtonEditProperty);
+//    Button addProperty = findViewById(R.id.floatingActionButtonAddProperty);
+//    Button removeProperty = findViewById(R.id.floatingActionButtonRemoveProperty);
+
+
+
+
 
     ListView listView;
     ArrayList<Property> properties = new ArrayList<Property>();
@@ -21,6 +37,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+//        editProperty.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                onAddButtonClicked();
+//            }
+//        });
+//
+//       addProperty.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "Add Property", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        removeProperty.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "Remove Property", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
         listView = (ListView) findViewById(R.id.listProperty);
 
         properties.add(Property.getInstanceOfProperty("cdc", "cdd"));
@@ -30,4 +68,33 @@ public class MainActivity extends AppCompatActivity {
         adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, properties);
         listView.setAdapter(adapter);
     }
-}
+
+//    private void onAddButtonClicked(){
+//        setVisibility(clicked);
+//        setAnimation(clicked);
+//        clicked = !clicked;
+//    }
+//
+//    private void setVisibility(boolean b){
+//        if(!clicked){
+//            addProperty.setVisibility(View.VISIBLE);
+//            removeProperty.setVisibility(View.VISIBLE);
+//        }
+//        else{
+//            addProperty.setVisibility(View.INVISIBLE);
+//            removeProperty.setVisibility(View.INVISIBLE);
+//        }
+//    }
+//
+//    private void setAnimation(boolean b){
+//        if(!clicked){
+//            addProperty.startAnimation(fromBottom);
+//            removeProperty.startAnimation(fromBottom);
+//            editProperty.startAnimation(rotateOpen);
+//        }
+//        else{
+//            addProperty.startAnimation(toBottom);
+//            removeProperty.startAnimation(toBottom);
+//            editProperty.startAnimation(rotateClose);
+//        }
+    }
